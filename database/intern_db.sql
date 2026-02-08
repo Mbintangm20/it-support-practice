94dd2b90ba7719e0ba9209e49f9fd0279a4f0abc
Bintang\Admin <mbintangmandala237956@gmail.com>
2026-02-09 00:27:02 +0700

    Add database schema

--- database/intern_db.sql
+++ database/intern_db.sql
@@ -1,11 +1,13 @@
-CREATE DATABASE intern_db;
-USE intern_db;
+CREATE DATABASE it_support;
+
+USE it_support;
 
 CREATE TABLE users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(50),
-  password VARCHAR(50)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50),
+    password VARCHAR(50),
+    role VARCHAR(20)
 );
 
-INSERT INTO users (username, password)
-VALUES ('admin', '12345');
+INSERT INTO users (username, password, role)
+VALUES ('admin', '12345', 'admin');
